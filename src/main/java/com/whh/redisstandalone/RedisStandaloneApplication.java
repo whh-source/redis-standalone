8e01cc40ae7ec1d2a415c8dcb78cfa4a6ad0a6e8
wanghuaihong1@konka.com <395055557@qq.com>
2020-01-21 10:09:32 +0800

    init project

---
+++ src/main/java/com/whh/redisstandalone/RedisStandaloneApplication.java
@@ -0,0 +1,16 @@
+package com.whh.redisstandalone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hipi
+ */
+@SpringBootApplication
+public class RedisStandaloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisStandaloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whh/redisstandalone/RedisStandaloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whh.redisstandalone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisStandaloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
